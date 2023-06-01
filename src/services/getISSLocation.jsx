030b3ce6d045d1311f5ad7a00322a69fefdd27cb
Santiag0Herrera <santi.nico.herrera@gmail.com>
2023-05-31 21:59:21 -0300

    proxy comment

--- src/services/getISSLocation.jsx
+++ src/services/getISSLocation.jsx
@@ -2,7 +2,7 @@
 export async function getISSLocation () {
     let ISSData = {}
     const API = {url: 'http://api.open-notify.org/iss-now.json', proxyURL: 'https://cors-anywhere.herokuapp.com/'}
-    const APIurl = API.proxyURL + API.url
+    const APIurl = API.proxyURL + API.url //we add the proxyURL so the API call can be made by HTTPS
     await fetch(APIurl)
         .then(res => res.json())
         .then(data => {
